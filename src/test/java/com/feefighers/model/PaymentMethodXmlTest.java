de095c5f51ab0770bdb34f42feed87dcb3f6cf76
Adam Siemion <adam.siemion@gmail.com>
2011-08-12 16:58:07 +0200

    added a payment method integration test

--- src/test/java/com/feefighers/model/PaymentMethodXmlTest.java
+++ src/test/java/com/feefighers/model/PaymentMethodXmlTest.java
@@ -11,7 +11,30 @@ import com.thoughtworks.xstream.mapper.CannotResolveClassException;
 
 public class PaymentMethodXmlTest {
 	
-	
+	@Test
+	public void shouldSerializePaymentMethod() throws Exception {
+		// given
+		final PaymentMethod paymentMethod = new PaymentMethod();
+		paymentMethod.setPaymentMethodToken("56f0c882c4565da74d2a748e");
+		paymentMethod.setCreatedAt(new Date());
+		paymentMethod.setUpdatedAt(new Date());
+		paymentMethod.setRetained(false);
+		paymentMethod.setRedacted(false);
+		paymentMethod.setSensitiveDataValid(false);		
+		paymentMethod.getMessageList().getList().add(new Message("error", "input.cvv", "too_long", ""));
+		paymentMethod.setLastFourDigits("1111");
+		paymentMethod.setFirstName("Joe");
+		paymentMethod.setLastName("FeeFighter");
+		paymentMethod.setExpiryMonth(1);
+		paymentMethod.setExpiryYear(2011);
+		final String xml = IOUtils.toString(getClass().getResourceAsStream("payment_method.xml"));		
+				
+		// when
+		final String outputXml = paymentMethod.toXml();
+		
+		// then
+		Assert.assertEquals(outputXml.trim(), xml.trim());		
+	}	
 	
 	@Test
 	public void shouldDeserializePaymentMethod() throws IOException {
