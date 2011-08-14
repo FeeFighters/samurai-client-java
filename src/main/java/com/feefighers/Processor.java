86bab8dfd6dbcd68a55ef12298cfada9007acf6d
Adam Siemion <adam.siemion@gmail.com>
2011-08-14 11:48:51 +0200

    added missing methods to Processor (only declarations, no bodies)

--- src/main/java/com/feefighers/Processor.java
+++ src/main/java/com/feefighers/Processor.java
@@ -1,10 +1,13 @@
 package com.feefighers;
 
 import com.feefighers.model.PaymentMethod;
+import com.feefighers.model.Options;
 
 public interface Processor {
 	
 	PaymentMethod find(String paymentMethodToken);
 			
+	void purchase(String paymentMethodToken, double amount, Options options);
 	
+	void authorize(String paymentMethodToken, double amount, Options options);
 }
--- src/main/java/com/feefighers/ProcessorImpl.java
+++ src/main/java/com/feefighers/ProcessorImpl.java
@@ -1,6 +1,7 @@
 package com.feefighers;
 
 import com.feefighers.http.Http;
+import com.feefighers.model.Options;
 import com.feefighers.model.PaymentMethod;
 
 public class ProcessorImpl implements Processor {
@@ -15,6 +16,20 @@ public class ProcessorImpl implements Processor {
 	public PaymentMethod find(String paymentMethodToken) {
 		String xml = http.get("/payment_methods/" + paymentMethodToken + ".xml");
 		return PaymentMethod.fromXml(xml);
+	}
+
+	@Override
+	public void purchase(String paymentMethodToken, double amount,
+			Options options) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void authorize(String paymentMethodToken, double amount,
+			Options options) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
