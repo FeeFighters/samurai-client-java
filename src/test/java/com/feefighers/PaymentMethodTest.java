96af13b81f0613c129e6002f3b7ec7fc794d23df
Adam Siemion <adam.siemion@gmail.com>
2011-08-14 15:07:39 +0200

    removed debug logging

--- src/test/java/com/feefighers/PaymentMethodTest.java
+++ src/test/java/com/feefighers/PaymentMethodTest.java
@@ -23,13 +23,11 @@ public class PaymentMethodTest {
 		config.load(getClass().getResourceAsStream("/config.properties"));
 		
 		final Http http = new Http(null, null, "https://samurai.feefighters.com/v1");
-		http.enableDebug();
 		final String output = http.post("payment_methods","redirect_url=http://localhost&" +
 				"merchant_key=" + config.getProperty("merchantKey") + "&" +
 				"credit_card[first_name]=Scooby&custom=&credit_card[last_name]=Do&credit_card[city]=Mystery Van&" +
 				"credit_card[state]=IL&credit_card[zip]=60607&credit_card[card_number]=4111111111111111&credit_card[cvv]=123&" +
 				"credit_card[expiry_month]=04&credit_card[expiry_year]=2014&sandbox=true");
-		System.out.println(output);
 		final Matcher matcher = Pattern.compile("payment_method_token=(.+)\\\"").matcher(output);
 		matcher.find();
 		final String paymentMethodToken = matcher.group(1);
