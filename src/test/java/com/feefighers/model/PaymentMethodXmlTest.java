6ebfd5fbf1aa6680c94358582db264289ef804ef
Adam Siemion <adam.siemion@gmail.com>
2011-08-14 15:08:09 +0200

    adjusted tests after model changes

--- src/test/java/com/feefighers/model/PaymentMethodXmlTest.java
+++ src/test/java/com/feefighers/model/PaymentMethodXmlTest.java
@@ -14,6 +14,9 @@ public class PaymentMethodXmlTest {
 	@Test
 	public void shouldSerializePaymentMethod() throws Exception {
 		// given
+		final Message message = new Message("input.cvv", "too_long", "");
+		message.messageClass = "error";
+		
 		final PaymentMethod paymentMethod = new PaymentMethod();
 		paymentMethod.setPaymentMethodToken("56f0c882c4565da74d2a748e");
 		paymentMethod.setCreatedAt(new Date());
@@ -21,7 +24,7 @@ public class PaymentMethodXmlTest {
 		paymentMethod.setRetained(false);
 		paymentMethod.setRedacted(false);
 		paymentMethod.setSensitiveDataValid(false);		
-		paymentMethod.getMessageList().getList().add(new Message("error", "input.cvv", "too_long", ""));
+		paymentMethod.getMessageList().getList().add(message);
 		paymentMethod.setLastFourDigits("1111");
 		paymentMethod.setFirstName("Joe");
 		paymentMethod.setLastName("FeeFighter");
